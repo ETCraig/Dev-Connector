4089c223571453b93e1588210a94c98efe1473dc
unknown <devethancraig@gmail.com>
2019-07-24 11:33:35 -0600

    Created Navbar && Landing Content && Brought them into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React, { Fragment } from 'react';
 import './App.css';
 
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+
 const App = () => {
   return (
     <Fragment>
-
+      <Navbar />
+      <Landing />
     </Fragment>
   );
 }
