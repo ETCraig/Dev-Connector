5dd7860dd7186515efc25412dc898bdc7208a144
unknown <devethancraig@gmail.com>
2019-07-25 11:49:07 -0600

    Alert Functionality w/ Register Finished -uuid bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import './App.css';
 
+import Aalert from './components/layout/Alert';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Navbar from './components/layout/Navbar';
@@ -18,6 +19,7 @@ const App = () => {
           <Navbar />
           <Route exact path='/' component={Landing} />
           <section className="container">
+            <Alert />
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,12 @@
 import React, { Fragment, useState } from 'react';
 
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-const Register = () => {
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
+
+const Register = ({ setAlert }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -17,7 +21,7 @@ const Register = () => {
     const onSubmit = async e => {
         e.preventDefault();
         if (password !== password2) {
-            console.log('No Match!');
+            setAlert('Passwords do not match', 'danger');
         } else {
             console.log(formData);
         }
@@ -80,4 +84,8 @@ const Register = () => {
     );
 }
 
-export default Register
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired
+};
+
+export default connect(null, { setAlert })(Register)
