d14338f04e1060186fa3b5e6f75ec3ba9cd9ea3a
unknown <devethancraig@gmail.com>
2019-07-26 11:33:57 -0600

    Created CreateProfile Component, State, Route, && onChange Functionality

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import './App.css';
 
 import Alert from './components/layout/Alert';
+import CreateProfile from './components/profile-form/CreateProfile';
 import Dashboard from './components/dashboard/Dashboard';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
@@ -35,6 +36,7 @@ const App = () => {
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute exact path='/create-profile' component={CreateProfile} />
             </Switch>
           </section>
         </Fragment>
