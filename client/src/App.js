2ed225231a60befba55fbb35bd1173d78d05f9ae
unknown <devethancraig@gmail.com>
2019-07-29 11:06:57 -0600

    Improved Routing && Finished Comments Functionality - Application Finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,10 @@
 import React, { Fragment, useEffect } from 'react';
 import './App.css';
 
-import AddEducation from './components/profile-form/AddEducation';
-import AddExperience from './components/profile-form/AddExperience';
-import Alert from './components/layout/Alert';
-import CreateProfile from './components/profile-form/CreateProfile';
-import Dashboard from './components/dashboard/Dashboard';
-import EditProfile from './components/profile-form/EditProfile';
 import Landing from './components/layout/Landing';
-import Login from './components/auth/Login';
 import { loadAccount } from './actions/Auth';
 import Navbar from './components/layout/Navbar';
-import Post from './components/post/Post';
-import Posts from './components/posts/Posts';
-import PrivateRoute from './components/routing/PrivateRoute';
-import Profile from './components/profile/Profile';
-import Profiles from './components/Profiles/Profiles';
-import Register from './components/auth/Register';
+import Routes from './components/routing/Routes';
 import SetAuthToken from './utils/SetAuthToken';
 import store from './store';
 
@@ -36,23 +24,10 @@ const App = () => {
       <Router>
         <Fragment>
           <Navbar />
-          <Route exact path='/' component={Landing} />
-          <section className="container">
-            <Alert />
-            <Switch>
-              <Route exact path='/register' component={Register} />
-              <Route exact path='/login' component={Login} />
-              <Route exact path='/profile/:id' component={Profile} />
-              <Route exact path='/profiles' component={Profiles} />
-              <PrivateRoute exact path='/dashboard' component={Dashboard} />
-              <PrivateRoute exact path='/create-profile' component={CreateProfile} />
-              <PrivateRoute exact path='/edit-profile' component={EditProfile} />
-              <PrivateRoute exact path='/add-experience' component={AddExperience} />
-              <PrivateRoute exact path='/add-education' component={AddEducation} />
-              <PrivateRoute exact path='/posts' component={Posts} />
-              <PrivateRoute exact path='/post/:id' component={Post} />
-            </Switch>
-          </section>
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route component={Routes} />
+          </Switch>
         </Fragment>
       </Router>
     </Provider>
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useEffect } from 'react';
 
 import CommentForm from './CommentForm';
+import CommentItem from './CommentItem';
 import { getPost } from '../../actions/Post';
 import PostItem from '../posts/PostItem';
 import Spinner from '../layout/Spinner';
@@ -23,6 +24,11 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
             </Link>
             <PostItem post={post} showActions={false} />
             <CommentForm postId={post._id} />
+            <div className='comments'>
+                {post.comments.map(comment => (
+                    <CommentItem key={comment._id} comment={comment} postId={post._id} />
+                ))}
+            </div>
         </Fragment>
     )
 }
