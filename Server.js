065fec8feccb5c721bc255f842c0eb4a27ab2e9f
unknown <devethancraig@gmail.com>
2019-07-29 11:49:46 -0600

    Preped for Heroku Deploy

--- Server.js
+++ Server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -14,6 +15,15 @@ app.use('/api/account', require('./routes/api/Account'));
 app.use('/api/auth', require('./routes/api/Auth'));
 app.use('/api/profile', require('./routes/api/Profile'));
 app.use('/api/posts', require('./routes/api/Posts'));
+
+//Serve Static Assets
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 8000;
 
